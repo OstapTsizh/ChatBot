2c08de86c13a7c5f99782534133daa145ac94020
Ostap Tsizh <ouzhazz@gmail.com>
2019-06-13 10:04:42 +0300

    Add project files.

---
+++ Template/Bot/Bot.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Template.Bot
+{
+    public class Bot : IBot
+    {
+        
+        public Bot()
+        {
+            
+        }
+
+        public async Task OnTurnAsync(ITurnContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Welcome user if it is new,
+            // start MainDialog.
+
+
+            // If current user continues dialog
+
+            string validatedUserText = GetUserText(context, context.Activity).ToString();
+            
+            //
+            // Dialog.Run(validatedUserText)
+
+        }
+
+        private async Task<IMessageActivity> GetUserText(ITurnContext context, Activity activity)
+        {
+            // Validation
+            // Errors
+            // Restart dialog
+            // ...
+
+            IMessageActivity validatedUserText = null;
+            return validatedUserText;
+        }
+    }
+}
