dc22810c5d89826b01cd8731f270299ecb1ce2b6
Misha Nadorozhniak <mnadorozhniak@gmail.com>
2019-06-24 12:45:51 +0300

    Added loader of Questions and Answers list

--- Bot.Core/Interfaces/IDecisionMaker.cs
+++ Bot.Core/Interfaces/IDecisionMaker.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Template.Core.Interfaces
 {
@@ -17,5 +19,38 @@ namespace Template.Core.Interfaces
         /// <returns>A dictionary with Topic, Keywords,
         /// question(s)/answer(s), decision(s).</returns>
         string GetQuestionOrResult(string topic);
+        List<DecisionModel> LoadQAList(); 
+    }
+
+    public class DecisionModel
+    {
+        // List<string> Topics ???
+
+        /// <summary>
+        /// A topic question is related to.
+        /// </summary>
+        public string Topic { get; set; }
+
+        /// <summary>
+        /// A question to ask user.
+        /// </summary>
+        public string Question { get; set; }
+
+        /// <summary>
+        /// Is this question from few sub-questions
+        /// to make next decision?
+        /// </summary>
+        public bool IsWaterfallQuestion { get; set; }
+
+        /// <summary>
+        /// Is this question final or we have next question in the tree?
+        /// </summary>
+        public bool HasNextQuestion { get; set; }
+
+        /// <summary>
+        /// A list of possible choices (answers) in
+        /// the body of a question for a user.
+        /// </summary>
+        public List<string> Options { get; set; }
     }
 }
