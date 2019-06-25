c96175c3ae314563039afb89db7c03be3d8292e4
Misha Nadorozhniak <mnadorozhniak@gmail.com>
2019-06-25 13:25:23 +0300

    Added GetDecision Method

---
+++ Bot.Core/Models/UserAnswersModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.Core.Models
+{
+    public class UserAnswersModel
+    {
+        public string[] Answers { get; set; }
+    }
+}
