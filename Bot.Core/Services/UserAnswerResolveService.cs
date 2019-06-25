15833d82ed3e11f5487a8d387b094843bcfd2983
Misha Nadorozhniak <mnadorozhniak@gmail.com>
2019-06-25 21:12:33 +0300

    Resolved problem with GetDecision Method

--- Bot.Core/Services/UserAnswerResolveService.cs
+++ Bot.Core/Services/UserAnswerResolveService.cs
@@ -13,9 +13,18 @@ namespace Template.Core.Services
     {
         public DecisionModel GetDecision(List<string> answers, QuestionModel questionModel)
         {
-            var decision = questionModel.Decisions.Where(d => d.Meta.Equals(answers.ToArray<string>())).First();
-            
-            return decision;
+            var Answers = answers.ToArray();
+            var decision = new DecisionModel();
+            for (var i = 0; i < questionModel.Decisions.Count; i++) 
+            {
+                decision = questionModel.Decisions[i];
+                if (decision.Meta.ToString() == Answers.ToString())
+                {
+                    return decision;
+                }
+            }
+
+            return null;
         }
     }
 }
